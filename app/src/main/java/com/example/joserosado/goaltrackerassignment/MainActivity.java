d229eafc63bdb27fcf5f735ae958004be52f4082
Destiny Zinke <dzinke@student.neumont.edu>
2019-05-20 12:36:54 -0600

    Week_Log

--- app/src/main/java/com/example/joserosado/goaltrackerassignment/MainActivity.java
+++ app/src/main/java/com/example/joserosado/goaltrackerassignment/MainActivity.java
@@ -1,13 +1,42 @@
 package com.example.joserosado.goaltrackerassignment;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button login_button = findViewById(R.id.login_button);
+
+
+        login_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                WeekListActivityIntent();
+            }
+        });
+    }
+
+    private void WeekListActivityIntent(){
+        Intent intent = new Intent(this,WeekListActivity.class);
+        startActivity(intent);
+        //Toast toast = Toast.makeText(getApplicationContext(),"Hits",Toast.LENGTH_SHORT);
+        //toast.show();
     }
 }
