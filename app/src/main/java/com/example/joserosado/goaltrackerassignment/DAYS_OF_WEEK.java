6523143e5902a10d91f24a94a9e376736df3c69b
Neondangel <jdiaz@student.neumont.edu>
2019-06-01 14:26:44 -0600

    Currently functioning + hopefully adds DAYS_OF_WEEK.java

---
+++ app/src/main/java/com/example/joserosado/goaltrackerassignment/DAYS_OF_WEEK.java
@@ -0,0 +1,11 @@
+package com.example.joserosado.goaltrackerassignment;
+
+public enum DAYS_OF_WEEK {
+    SUNDAY,
+    MONDAY,
+    TUESDAY,
+    WEDNESDAY,
+    THURSDAY,
+    FRIDAY,
+    SATURDAY
+}
