07137574cc734eb8cabfe2c8d3d817064c9844ce
BGnger <bthompson@student.neumont.edu>
2019-06-01 17:58:15 -0600

    Login to Firebase Authentication works All FirebaseManager interface uses Google Task API (except SignOut)

---
+++ app/src/main/java/com/example/joserosado/goaltrackerassignment/GoalTracker/db/FirebaseExecutor.java
@@ -0,0 +1,10 @@
+package com.example.joserosado.goaltrackerassignment.GoalTracker.db;
+
+import java.util.concurrent.Executor;
+
+public class FirebaseExecutor implements Executor {
+    @Override
+    public void execute(Runnable runnable) {
+
+    }
+}
